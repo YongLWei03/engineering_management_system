0d60c2967a0e5a86c4c0ba62d3b7b759c84475ef
yangle <18601036905@163.com>
2016-08-19 23:38:35 +0800

    better

--- ems.py
+++ ems.py
@@ -1,8 +1,11 @@
 # --*-- coding:utf-8 --*--
 
 from flask import Flask, request
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
