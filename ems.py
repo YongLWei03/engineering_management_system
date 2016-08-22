682a2bf8c5b8bef00e74348411619da93e3f0cc1
yangle <18601036905@163.com>
2016-08-22 21:54:49 +0800

    better

--- ems.py
+++ ems.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,6 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
@@ -21,4 +23,4 @@ def index():
     return render_template("login.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
