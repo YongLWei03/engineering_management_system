98e28df4230ff6f3a59a9b2d155d9c99982f828c
yangle <18601036905@163.com>
2016-08-23 21:25:28 +0800

    better

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,12 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
+    print config[config_name]
+
     bootstrap.init_app(app)
     db.init_app(app)
+
+    print db
 
     from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
