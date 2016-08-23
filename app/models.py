a7255e1baf7f6e87e25c1458fe8145cc76872ca2
yangle <18601036905@163.com>
2016-08-23 21:59:02 +0800

    better

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@
 
 from . import db
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Role(db.Model):
     __tablename__ = "roles"
@@ -17,3 +19,15 @@ class User(db.Model):
     nickname = db.Column(db.String(64))
     companyId = db.Column(db.Integer)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError("密码是不可读的属性")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
