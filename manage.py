dd8e159784897fe080ad81467d46c4a21cdbe2a7
yangle <18601036905@163.com>
2016-08-23 21:45:20 +0800

    better

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 # --*-- coding:utf-8 --*--
 
 import os
-from app import create_app, db
 from app.models import User, Role
+from app import create_app, db
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
