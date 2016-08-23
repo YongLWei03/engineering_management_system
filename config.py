e01a8896d6addb78fd0e9cb592cb471eef58fdbb
yangle <18601036905@163.com>
2016-08-23 21:27:40 +0800

    better

--- config.py
+++ config.py
@@ -4,18 +4,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class config:
+class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "hard to guess string"
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
-class DevelopmentConfig(config):
+class DevelopmentConfig(Config):
     DEBUG = True
     SQLAlCHEMY_DATABASE_URI = (os.environ.get("DEV_DATABASE_URL") or "sqlite:///" + os.path.join(basedir, 'data-dev.sqlite'))
 
 
-class ProductionConfig(config):
+class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = (os.environ.get("DATABASE_URL") or "sqlite:///" +
                                os.path.join(basedir, "data.sqlite"))
 
