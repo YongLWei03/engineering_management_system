590df5c160a05fb62962684ee26df1cf90042065
yangle <18601036905@163.com>
2016-08-25 22:24:20 +0800

    wt

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,6 +10,8 @@ class BaseForm(Form):
 
 
 class Login_form(BaseForm):
-    account = StringField(validators=[Required(u"不能为空"), Email(u"邮箱格式不正确")])
-    password = PasswordField(validators=[Required(u"不能为空")])
+    account = StringField(u"请输入邮箱账号",
+                          validators=[Required(u"不能为空"),
+                                      Email(u"邮箱格式不正确")])
+    password = PasswordField(u"请输入密码", validators=[Required(u"不能为空")])
     submit = SubmitField(u"登录")
