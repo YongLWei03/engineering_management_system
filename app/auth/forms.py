3e72e077a0b8047308dc55d909ff6126c4be3f94
yangle <18601036905@163.com>
2016-08-25 22:19:11 +0800

    wt

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,7 +2,7 @@
 
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import Required, Email
 
 
 class BaseForm(Form):
@@ -11,7 +11,7 @@ class BaseForm(Form):
 
 class Login_form(BaseForm):
     account = StringField(u"请输入邮箱账号",
-                          validators=[InputRequired(u"不能为空"),
+                          validators=[Required(u"不能为空"),
                                       Email(u"邮箱格式不正确")])
-    password = PasswordField(u"请输入密码", validators=[InputRequired(u"不能为空")])
+    password = PasswordField(u"请输入密码", validators=[Required(u"不能为空")])
     submit = SubmitField(u"登录")
