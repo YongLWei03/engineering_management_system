90cdc51a7910aaab54137953e602812d82b8cb0d
yangle <18601036905@163.com>
2016-08-25 22:17:38 +0800

    wt

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,8 +5,13 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Email
 
 
-class Login_form(Form):
+class BaseForm(Form):
+    LANGUAGES = ['zh']
+
+
+class Login_form(BaseForm):
     account = StringField(u"请输入邮箱账号",
-                          validators=[InputRequired(u"不能为空"), Email(u"邮箱格式不正确")])
+                          validators=[InputRequired(u"不能为空"),
+                                      Email(u"邮箱格式不正确")])
     password = PasswordField(u"请输入密码", validators=[InputRequired(u"不能为空")])
     submit = SubmitField(u"登录")
