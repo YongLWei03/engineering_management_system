f1c9b8400090ec5de517a019e797e4b285a6f02c
yangle <18601036905@163.com>
2016-08-28 15:13:15 +0800

    better

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 # --*-- coding:utf-8 --*--
 
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Required, Email
 
 
@@ -10,8 +10,7 @@ class BaseForm(Form):
 
 
 class Login_form(BaseForm):
-    account = StringField(u"请输入邮箱账号",
-                          validators=[Required(u"不能为空"),
-                                      Email(u"邮箱格式不正确")])
+    email = StringField(u"请输入邮箱账号", validators=[Required(u"不能为空"), Email(u"邮箱格式不正确")])
     password = PasswordField(u"请输入密码", validators=[Required(u"不能为空")])
+    remember_me = BooleanField(u"记住我")
     submit = SubmitField(u"登录")
