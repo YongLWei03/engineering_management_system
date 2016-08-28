ecd0156ef9e905cc318f74eabbf11f7a70b3f823
yangle <18601036905@163.com>
2016-08-28 16:58:38 +0800

    better

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import g, render_template
 from flask_login import login_required
 
 from . import admin
@@ -9,4 +9,6 @@ from . import admin
 @admin.route("/", methods=["GET", "POST"])
 @login_required
 def index():
+    print 'g'*100
+    print g.user
     return render_template("admin/index.html")
