50f94cf67f32da24f75d88f81ea8a19a000b39f9
yangle <18601036905@163.com>
2016-08-29 21:07:34 +0800

    ztree

--- app/staff/views.py
+++ app/staff/views.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from . import staff
 
@@ -9,4 +9,5 @@ from . import staff
 @staff.route("/department/", methods=["GET", "POST"])
 @login_required
 def department():
-    return render_template("staff/department.html")
+    user = current_user
+    return render_template("staff/department.html", user=user)
