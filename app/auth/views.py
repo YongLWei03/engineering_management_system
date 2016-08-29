2d9a5f152316d9e96bea8ffde09e03073f45a8b8
yangle <18601036905@163.com>
2016-08-29 22:14:54 +0800

    better

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,14 +11,8 @@ from ..models import User
 @auth.route("/login", methods=["GET", "POST"])
 def login():
     form = Login_form()
-    print 'f'*100
-    print form.validate_on_submit()
-    print form.email.data
-    print form.password.data
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        print user
-        print user.verify_password(form.password.data)
         if user and user.verify_password(form.password.data):
             login_user(user)
             return redirect(request.args.get("next") or url_for("admin.index"))
