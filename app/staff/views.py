29c42ac1d61d2574f94fed1fb1ce4706a8230612
yangle <18601036905@163.com>
2016-08-29 21:13:57 +0800

    ztree

--- app/staff/views.py
+++ app/staff/views.py
@@ -10,4 +10,4 @@ from . import staff
 @login_required
 def department():
     user = current_user
-    return render_template("staff/department.html", user=user)
+    return render_template("admin/index.html", user=user)
