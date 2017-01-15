b9f9f5845a7b229094eeb04584803d14bae90b18
yangle <18601036905@163.com>
2017-01-15 22:28:20 +0800

    better

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,17 +4,15 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user
 
 from . import auth
-from .forms import Login_form
 from ..models import User
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    form = Login_form()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and user.verify_password(form.password.data):
             login_user(user)
             return redirect(request.args.get("next") or url_for("admin.index"))
         flash("用户名或者密码错误")
-    return render_template("auth/login.html", form=form)
+    return render_template("auth/login.html")
