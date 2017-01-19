cf5fdb17b9f6945342a9a6974e6f6798ae2b2fdc
yangle <18601036905@163.com>
2017-01-19 23:00:45 +0800

    better

--- app/models.py
+++ app/models.py
@@ -89,7 +89,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class Equipment(db.Model):
-    __tablename_- = 'equipments'
+    __tablename__ = 'equipments'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     picture =db.Column(db.String(64))
