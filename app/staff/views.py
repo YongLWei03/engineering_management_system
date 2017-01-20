40a1e119cf7ff499cb2598f376fd7e3f38fb92c0
yangle <18601036905@163.com>
2017-01-21 00:23:54 +0800

    better

--- app/staff/views.py
+++ app/staff/views.py
@@ -50,6 +50,11 @@ def add_staff():
     elif request.method == "POST":
         staff_id = request.form.get('staff_id')
         if staff_id:
+            update_dict = request.form.to_dict()
+            update_dict.pop('staff_id')
+            staff_id = int(staff_id)
+            db.session.query(User).filter(
+                User.id == staff_id).update(update_dict)
             print 'r'*100
             print request.form
             print dir(request.form)
